8326992271d4990f0b308fc76cfbf4d32eb43531
Sebastian Alvarez <alvarezsebas552@gmail.com>
2023-02-18 18:49:51 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CoreEscuela{
+    class Escuela{
+        public string nombre;
+        public string dirrecion;
+        public int AñoFundacion;   
+        public string ceo;
+        public void Timbrar(){
+            //Todo
+            Console.Beep(2000, 3000);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args){
+
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Hodwards";
+            miEscuela.dirrecion = "Muy Lejos";
+            miEscuela.AñoFundacion = 2000;
+            Console.WriteLine("QUE SUENE PERROS");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello world");
+        }
+    }
+}
